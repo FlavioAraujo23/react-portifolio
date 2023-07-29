ea7159ab0d579e7fb8f2fdaff0caf7db1f5702b9
FlavioAraujo23 <flaviolopes1027@gmail.com>
2023-07-28 23:38:06 -0300

    add section projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,5 @@
+import { HiArrowTopRightOnSquare } from "react-icons/hi2"
+
 export function Projects() {
   const projects = [
     {
@@ -9,10 +11,10 @@ export function Projects() {
       colSpan: "col-span-1",
     },
     {
-      title: "Projeto 2",
-      description: "Descrição do projeto 2",
+      title: "Bikcraft",
+      description: "Site fictício de bicicletas elétricas, criado como projeto final do curso de HTML e CSS para Iniciantes - Origamid. Neste projeto trabalhei com CSS, HTML e JavaScript.",
       image:
-        "colocar imagem url",
+        "./assets/img/bikcraft-mockup.jpg",
       link: "#",
       colSpan: "col-span-1 md:col-span-2",
     },
@@ -44,7 +46,44 @@ export function Projects() {
 
   return (
     <>
-    
+       <section className="rounded-tl-[80px] bg-gradient-to-tr from-black to-gray-900 text-white md:rounded-tl-[180px]">
+        <div className="container mx-auto max-w-4xl p-4 py-12">
+          <div className="relative p-4 text-center">
+            <h2 className="relative z-50 mb-2 text-white">
+              <span className="mr-2 font-headline text-3xl font-semibold">
+                Projetos &
+              </span>
+              <span className="font-handwriting text-4xl">Portfólio</span>
+            </h2>
+            <p className="relative text-sm text-gray-400">
+              Alguns dos projetos pessoais e que já realizei ao longo da minha
+              trajetória como programador front-end.
+            </p>
+            <div className="absolute left-1/2 top-3 z-0 h-10 w-10 rounded-lg bg-blue-400/10" />
+          </div>
+
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+            {projects.map((project, index) => (
+              <div
+                key={index}
+                className={`group relative h-52 cursor-default rounded-lg ${project.colSpan} bg-cover bg-center`}
+                style={{ backgroundImage: `url('${project.image}')` }}
+              >
+                <div className="absolute inset-0 flex h-full w-full flex-col items-center justify-center rounded-lg bg-blue-600 text-white opacity-0 transition-opacity group-hover:opacity-100">
+                  <h4 className="font-headline text-lg font-semibold">
+                    {project.title}
+                  </h4>
+                  <p className=" mb-4 ml-2 text-sm">{project.description}</p>
+                  <a href={project.link} target="_blank">
+                    <HiArrowTopRightOnSquare className="h-6 w-6" />
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <div className="absolute right-0 -mt-[6px] h-3 w-48 rounded-l-full bg-gradient-to-r from-gray-700 to-gray-600 md:w-96" />
     </>
   )
 }
