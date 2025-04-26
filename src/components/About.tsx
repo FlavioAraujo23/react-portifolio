46e878c5be1109b674f1abef31cba9619dcfbff2
FlavioAraujo23 <flaviolopes1027@gmail.com>
2025-04-26 18:45:38 -0300

    update: some info in project

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 export function About() {
   return (
     <section className="container mx-auto my-4 max-w-5xl p-4">
-      <div className="p-4 text-center relative">
-        <h2 className="text-blue-900 font-bold relative z-10">
+      <div className="relative p-4 text-center">
+        <h2 className="relative z-10 font-bold text-blue-900">
           <span className="mr-2 font-headline text-3xl sm:text-2xl">Sobre</span>
           <span className="font-handwriting text-4xl sm:text-3xl">Mim</span>
         </h2>
@@ -10,16 +10,18 @@ export function About() {
           Tenho executado projetos e gostaria que o próximo seja o seu.
         </p>
 
-        <div className="absolute left-1/2 top-3 z-0 h-10 w-10 rounded-lg bg-blue-100/40"/>
+        <div className="absolute left-1/2 top-3 z-0 h-10 w-10 rounded-lg bg-blue-100/40" />
       </div>
 
       <div className="relative mx-auto mt-20 max-w-lg">
-        <div className="bg-blue-100 relative w-full rounded-lg p-4 ps-20 md:h-64 md:ps-48">
+        <div className="relative w-full rounded-lg bg-blue-100 p-4 ps-20 md:h-64 md:ps-48">
           <div className="relative h-full w-full rounded-lg bg-gray-50 p-4">
             <p className="font-handwriting text-lg font-bold">Olá,</p>
             <p>
               <span className="mr-1">Meu nome é</span>
-              <span className="font-headline font-bold uppercase text-blue-900">Flávio Araújo</span>
+              <span className="font-headline font-bold uppercase text-blue-900">
+                Flávio Araújo
+              </span>
             </p>
 
             <table className="mt-4 w-full text-xs">
@@ -28,20 +30,7 @@ export function About() {
                   <td className="font-headline font-bold uppercase text-blue-900 ">
                     Idade:
                   </td>
-                  <td>20</td>
-                </tr>
-                <tr>
-                  <td className="font-headline font-bold uppercase text-blue-900">
-                    Celular:
-                  </td>
-                  <td>
-                    <a
-                      href="tel:+5511960943672"
-                      className="underline hover:text-blue-800"
-                    >
-                      +55 11 9.6094-3672
-                    </a>
-                  </td>
+                  <td>22</td>
                 </tr>
                 <tr>
                   <td className="font-headline font-bold uppercase text-blue-900">
@@ -84,10 +73,9 @@ export function About() {
               </tbody>
             </table>
           </div>
-          <div className="absolute -left-2 -top-4 h-24 w-20 rounded-lg bg-gray-600 bg-[url('https://cdn.dribbble.com/userupload/8932548/file/original-f92ffb8df782ef9392a1e48b1b955527.jpg?resize=1024x1280')] bg-cover bg-center md:-left-12 md:-top-12 md:h-72 md:w-56">
-          </div>
+          <div className="absolute -left-2 -top-4 h-24 w-20 rounded-lg bg-gray-600 bg-[url('https://cdn.dribbble.com/userupload/8932548/file/original-f92ffb8df782ef9392a1e48b1b955527.jpg?resize=1024x1280')] bg-cover bg-center md:-left-12 md:-top-12 md:h-72 md:w-56"></div>
         </div>
       </div>
     </section>
-  )
+  );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,37 +1,52 @@
-import { HiDownload } from 'react-icons/hi'
+import { HiDownload } from "react-icons/hi";
 export function Hero() {
   return (
     <>
-        <section className="rounded-br-[80px] bg-gradient-to-tr from-black to-gray-900 text-white md:rounded-br-[180px]">
-        <div className="container mx-auto max-w-4xl p-2 py-12 flex flex-col md:flex-row">
-            <div className='basis-1/2'>
-            <h1 className="mb-6 text-center md:text-left"> 
-          <span className="font-handwriting block text-3xl text-center md:text-left">Olá, me chamo</span> 
-          <span className="font-headline text-5xl font-semibold">Flávio</span> 
-          <span className="font-headline text-5xl font-light text-blue-400 ml-1">Araújo</span>
-          </h1>
-          <h2 className="font-semibold flex items-center gap-2 text-center md:text-left">
-            <div className="h-1 w-12 rounded-full bg-blue-800" />
-            Desenvolvedor React.js Front-end
+      <section className="rounded-br-[80px] bg-gradient-to-tr from-black to-gray-900 text-white md:rounded-br-[180px]">
+        <div className="container mx-auto flex max-w-4xl flex-col p-2 py-12 md:flex-row">
+          <div className="basis-1/2">
+            <h1 className="mb-6 text-center md:text-left">
+              <span className="block text-center font-handwriting text-3xl md:text-left">
+                Olá, me chamo
+              </span>
+              <span className="font-headline text-5xl font-semibold">
+                Flávio
+              </span>
+              <span className="ml-1 font-headline text-5xl font-light text-blue-400">
+                Araújo
+              </span>
+            </h1>
+            <h2 className="flex items-center gap-2 text-center font-semibold md:text-left">
+              <div className="h-1 w-12 rounded-full bg-blue-800" />
+              Desenvolvedor React.js Front-end
             </h2>
-            <p className="text-gray-400 my-6 text-center md:text-left">
+            <p className="my-6 text-center text-gray-400 md:text-left">
               Transformando ideias em interfaces inovadoras!
             </p>
-            <div className="flex items-center gap-2 justify-center md:justify-start">
-              <a href="mailto:flaviolopes1027@gmail.com" className="underline font-bold text-white">Fale Comigo</a>
+            <div className="flex items-center justify-center gap-2 md:justify-start">
+              <a
+                href="mailto:flaviolopes1027@gmail.com"
+                className="font-bold text-white underline"
+              >
+                Fale Comigo
+              </a>
               <span className="italic text-gray-500">ou</span>
-              <a href="/download/CV-FlavioAraujo.pdf" download={"CV-FlavioAraujo.pdf"} className="button text-gray-600 hover:text-gray-900 flex items-center gap-2">
+              <a
+                href="/download/CV__Flavio__Araujo.pdf"
+                download={"CV__Flavio__Araujo.pdf"}
+                className="button flex items-center gap-2 text-gray-600 hover:text-gray-900"
+              >
                 <HiDownload />
-                Baixe meu CV</a>
-            </div>
-            </div>
-            <div className='basis-1/2'>
-              <div className=" bg-cover bg-center md:-left-12 md:-top-12 md:h-72 md:w-56 rounded-xl"></div> 
+                Baixe meu CV
+              </a>
             </div>
           </div>
+          <div className="basis-1/2">
+            <div className=" rounded-xl bg-cover bg-center md:-left-12 md:-top-12 md:h-72 md:w-56"></div>
+          </div>
+        </div>
       </section>
       <div className="absolute left-0 -mt-2 h-3 w-1/4 rounded-r-full bg-gradient-to-r from-blue-700 to-blue-600 md:w-1/3"></div>
     </>
-  
-  )
+  );
 }
